150264a6aa0a615ae01beee64b466f53719ad98d
Stu Fletcher <mail@stuliveshere.com>
2014-09-28 20:58:09 +1000

    added prac1 template

--- src/file_based_processing.py
+++ src/file_based_processing.py
@@ -14,23 +14,20 @@ from matplotlib.widgets import Slider
 
 
 def io(func):
-
-
+	'''
+	an io decorator that allows
+	input/output to be either a filename 
+	(i.e. a string) or an array
 	def wrapped(*args, **kwargs) :
-		
 		if type(args[0]) == type(''):
 			kwargs['dataset'] = toolbox.read(args[0])
 		else:
 			kwargs['dataset'] = args[0]
-		
 		result = func(**kwargs)
-			
 		if type(args[1]) == type(''):
-			
 			return toolbox.write(result, args[1])
 		else:
 			return result
-		
 	return wrapped
 
 
