83d924f85b52a9e774b69008fcab1c6aa7dce0aa
Stu Fletcher <mail@stuliveshere.com>
2014-10-07 10:30:17 +1000

    added multiprocessing

--- src/prac1/exersize6.py
+++ src/prac1/exersize6.py
@@ -1,24 +1,31 @@
 from toolbox import io
 import toolbox
 import numpy as np
+from multiprocessing import Pool
 import matplotlib.pyplot as pylab
 from exersize1 import initialise
 from exersize5 import build_combined, add_noise, convolve_wavelet
 
+def run(sx):
+	dataset, kwargs = initialise()
+	print sx
+	dataset['sx'] = sx
+	kwargs['sx'] = sx
+	dataset['offset'] = dataset['gx'] - dataset['sx']
+	kwargs['aoffsets'] = np.abs(dataset['offset'])	
+	dataset['trace'].fill(0)
+	dataset = build_combined(dataset, None, **kwargs)
+	dataset = convolve_wavelet(dataset, None, **kwargs)
+	dataset = add_noise(dataset, None, **kwargs)
+	#need to add cdp locations to dataset
+	toolbox.cp(dataset, 'shot%d.su' %sx, **kwargs)	
+
 if __name__ == "__main__":
-	workspace, param = initialise()
-	
-	for sx in param['sx_coords']:
-		print sx
-		workspace['sx'] = sx
-		param['sx'] = sx
-		workspace['offset'] = workspace['gx'] - workspace['sx']
-		param['aoffsets'] = np.abs(workspace['offset'])	
-		workspace['trace'].fill(0)
-		workspace = build_combined(workspace, None, **param)
-		workspace = convolve_wavelet(workspace, None, **param)
-		workspace = add_noise(workspace, None, **param)
-		#need to add cdp locations to workspace
-		toolbox.cp(workspace, 'shot%d.su' %sx, **param)
+	#~ workspace, param = initialise()
+	#~ pool = Pool(processes=8)
+	#~ result = pool.map(run, param['sx_coords'])
+	workspace = toolbox.agc('./su/shot201.su', None, None)
+	toolbox.display(workspace, None, None)
+
 		
 		
