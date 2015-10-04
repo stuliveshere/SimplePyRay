90c0f30c177e07e83b365373cd59ef8bf602a552
Stu Fletcher <mail@stuliveshere.com>
2015-10-04 23:03:46 +1000

    pulled out SU

--- src/prac1/exersize1.py
+++ src/prac1/exersize1.py
@@ -8,53 +8,53 @@ import numpy as np
 
 @io
 def spike(dataset, **kwargs):
-	dataset['trace'][:,500] = 1
-	return dataset
+        dataset[:,500] = 1
+
 
 #-----------------------------------------------------------------------
 #              main functions
 #-----------------------------------------------------------------------
 
 def initialise():
-	parameters = {}
-	#build our model, which is pre-defined in the toolbox
-	parameters['model'] = toolbox.build_model()
-	#have a look at it - it has a build in display routine
-	#~ parameters['model'].display()
-	
-	#initialise data workspace
-	parameters['sutype'] = toolbox.typeSU(1000)
-	workspace = np.zeros(500, dtype=parameters['sutype'])	
-	
-	#define survey geometry, ie shot and reciever points
-	parameters['sx_coords'] = np.arange(500.0)[::2] + 1
-	workspace['gx'] = np.arange(500.0)+1
-	parameters['gx'] = np.arange(500.0)
-	
-	#add some more useful stuff
-	workspace['ns'] = 1000
-	workspace['dt'] = 1000 #* 1e-6
-	parameters['dt'] = 1e-3	
-	parameters['sz'] = 0
-	parameters['gz'] = 0
-	parameters['nx'] = workspace['gx'].size	
-	return workspace, parameters
-	
+        parameters = {}
+        #build our model, which is pre-defined in the toolbox
+        parameters['model'] = toolbox.build_model()
+       
+        nx = parameters['nx'] = parameters['model']['nx']
+        nz = parameters['nz'] = parameters['model']['nz']
+        
+        #initialise data workspace
+        workspace = np.zeros((nx, nz), dtype=np.float32)	
+        
+        #define survey geometry, ie shot and reciever points
+        parameters['sx'] = 250
+        parameters['gx'] = np.arange(500.0)
+        
+        #add some more useful stuff
+        parameters['dt'] = 1e-3	
+        parameters['sz'] = 0
+        parameters['gz'] = 0
+        return workspace, parameters
+        
 if __name__ == '__main__':
-	workspace, param = initialise()
-	spike(workspace, None, **param)
-	toolbox.display(workspace, None, **param)
-	
+        workspace, params = initialise()
+        #check dictionary contents
+        print params['model'].keys()
+        #have a look at it - it has a build in display routine
+        params['model'].display()
+        spike(workspace, None, **params)
+        toolbox.display(workspace, None, **params)
 
-		
+
+                
 
 
-		
-		
-	
-	
-	
+                
+                
+        
+        
+        
 
-	
-	
-	
+        
+        
+        
--- src/prac2/exercise8.py
+++ src/prac2/exercise8.py
@@ -26,30 +26,30 @@ if __name__ == "__main__":
         #intialise workspace and parameter dictionary
         workspace, params = initialise('survey.su')
         
-        #set our TAR
-        params['gamma'] = 10
-        tar(workspace, None, **params)
+        #~ #set our TAR
+        #~ params['gamma'] = 10
+        #~ tar(workspace, None, **params)
         
        
-        #apply LMO
-        params['lmo'] =2200.0
-        lmo(workspace, None, **params)
-        workspace['trace'][:,80:110] *= 0
-        params['lmo'] = -2200.0
-        lmo(workspace, None, **params)
+        #~ #apply LMO
+        #~ params['lmo'] =2200.0
+        #~ lmo(workspace, None, **params)
+        #~ workspace['trace'][:,80:110] *= 0
+        #~ params['lmo'] = -2200.0
+        #~ lmo(workspace, None, **params)
         
-        #apply our NMO
-        params['smute'] = 100.0
-        v = [1417, 1510,1878]
-        t = [0.171, 0.215, 0.381]
-        params['vels'] = toolbox.build_vels(t, v)
-        nmo(workspace, None, **params)
+        #~ #apply our NMO
+        #~ params['smute'] = 100.0
+        #~ v = [1417, 1510,1878]
+        #~ t = [0.171, 0.215, 0.381]
+        #~ params['vels'] = toolbox.build_vels(t, v)
+        #~ nmo(workspace, None, **params)
         
-        #apply AGC
-        toolbox.agc(workspace, None, **params)
+        #~ #apply AGC
+        #~ toolbox.agc(workspace, None, **params)
         
-        #stack
-        stack(workspace, 'stack1.su', **params)
+        #~ #stack
+        #~ stack(workspace, 'stack1.su', **params)
         
         #view
         toolbox.display('stack1.su', None, **params)
