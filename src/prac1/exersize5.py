53973db9490fba8751178b7f145600b70a4b573c
Stu Fletcher <mail@stuliveshere.com>
2015-10-05 18:39:31 +1000

    refactored ex5

--- src/prac1/exersize5.py
+++ src/prac1/exersize5.py
@@ -13,39 +13,33 @@ from exersize4 import build_reflector
 
 @io
 def build_combined(dataset, **kwargs):
-	dataset = build_direct(dataset, None, **kwargs)
-	dataset = build_refractor(dataset, None, **kwargs)
-	dataset = build_reflector(dataset, None, **kwargs)
-	return dataset
-	
+        dataset = build_direct(dataset, None, **kwargs)
+        dataset = build_refractor(dataset, None, **kwargs)
+        dataset = build_reflector(dataset, None, **kwargs)
+        return dataset
+        
 @io
 def add_noise(dataset, **kwargs):
-	noise = np.random.normal(0.0, 1e-8, size=(dataset['trace'].shape))
-	dataset['trace'] += noise
-	return dataset
-	
+        noise = np.random.normal(0.0, 1e-8, size=(dataset.shape))
+        dataset += noise
+        return dataset
+        
 @io
 def convolve_wavelet(dataset, **kwargs):
-	wavelet = toolbox.ricker(60)	
-	dataset =  toolbox.conv(dataset, wavelet)
-	return dataset
+        wavelet = toolbox.ricker(60)	
+        dataset =  toolbox.conv(dataset, wavelet)
+        return dataset
 
 if __name__ == '__main__':
-	workspace, param = initialise()
-	
-	sx = 100
+        workspace, params = initialise()
+        
 
-	workspace['sx'] = sx
-	param['sx'] = sx
-	workspace['offset'] = workspace['gx'] - workspace['sx']
-	param['aoffsets'] = np.abs(workspace['offset'])
-	
-	#build record
-	workspace = build_combined(workspace, None, **param)
-	
-	#build wavelet
-	convolve_wavelet(workspace, 'test.su', **param)
-
-	workspace = toolbox.agc(workspace, None, **param)
-	#~ workspace = add_noise(workspace, 'record.su', **param)
-	toolbox.display(workspace, None, **param)
+        #build record
+        build_combined(workspace, None, **params)
+        
+        #build wavelet
+        workspace = convolve_wavelet(workspace, None, **params)
+        workspace = add_noise(workspace, None, **params)
+        toolbox.agc(workspace, None, **params)
+        
+        toolbox.display(workspace, None, **params)
